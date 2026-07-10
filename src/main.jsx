37802320ecce00d53651fabdc875652b70fee0c6
MarcosLopezRodriguez <marcos.lopezrod@gmail.com>
2026-07-10 09:43:47 +0200

    fix: open extension popup from toolbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 
+if (globalThis.location?.protocol === "chrome-extension:") {
+  document.documentElement.dataset.shell = "extension";
+}
+
 createRoot(document.getElementById("root")).render(<App />);
